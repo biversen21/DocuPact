68221179ea3c8600a1fcaaba962223ec0f2672c1
Brian Iversen <brian.iversen@docusign.com>
2015-10-06 12:26:50 -0700

    Add dummy data json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+// var faker = require('faker');
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
@@ -13,3 +14,19 @@ new WebpackDevServer(webpack(config), {
 
   console.log('Listening at localhost:3000');
 });
+
+// var fakeData = function() {
+// 	var jsonData = {
+// 		users: [],
+// 	}
+// 	for (var i = 0; i < 2000; i++) {
+// 		newUser = {};
+// 		newUser.name = faker.name.findName();
+// 		newUser.hours = faker.random.number(30)
+// 		newUser.department = faker.name.jobArea()
+// 		newUser.locationOfWork = faker.company.companyName()
+// 		jsonData.users.push(newUser);
+// 	}
+// 	console.log (JSON.stringify(jsonData));
+// }
+// fakeData();
